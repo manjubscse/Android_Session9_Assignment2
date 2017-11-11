aa910302cd9eba90ea7e5fce5ce161bc626b3cb9
Manju B S <manjubscse@gmail.com>
2017-11-11 17:20:41 +0530

    Session9_Assignment2

---
+++ app/src/main/java/com/manjubs/SubMenuWithToasts/MenuList.java
@@ -0,0 +1,18 @@
+package com.manjubs.SubMenuWithToasts;
+
+/**
+ * Created by User on 27/09/2017.
+ */
+
+// Create Menu List Class
+public class MenuList {
+
+    // And Enum of those Menu Items shall be Displayed in Options Menu.
+    public enum OptionMenuItems{
+        Computer,
+        Gamepad,
+        Camera,
+        Video,
+        Email
+    }
+}
